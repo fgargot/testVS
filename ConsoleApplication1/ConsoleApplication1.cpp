8a4f0f966c1f00bad62d96c371e4c6edc1d4877c
Florent Gargot <florent.gargot@outlook.com>
2015-11-25 18:16:20 +0100

    Test

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,15 @@
+// ConsoleApplication1.cpp : définit le point d'entrée pour l'application console.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello world!" << endl;
+    return 0;
+}
+
